768a984fb8f27f60c82d22f16a45dbf57e300d38
RV Antonisse <info@rv-antonisse.nl>
2014-11-18 15:49:57 +0100

    task 3

---
+++ exercise1/lib/string-helper.js
@@ -0,0 +1,23 @@
+module.exports = (function () {
+	var stringHelper = {};
+
+	stringHelper.titleize = function (string) {
+		console.log('titleize()', string);
+		if (typeof string !== 'string') {
+			string = string.toString();
+		}
+		var newString = '',
+			stringArray = string.split(' ');
+		console.log(stringArray);
+		for (var i in stringArray) {
+			var word = stringArray[i],
+				firstLetter = stringArray[i].substr(0,1);
+			word = firstLetter.toUpperCase() + word.substr(1);
+			stringArray[i] = word;
+		}
+		newString = stringArray.join(' ');
+		return newString;
+	};
+
+	return stringHelper;
+}());
