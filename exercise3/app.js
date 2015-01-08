2f42d8e71d85d30b538e51997563ea7cb4dafd7d
RV Antonisse <info@rv-antonisse.nl>
2015-01-08 13:27:10 +0100

    final task

--- exercise3/app.js
+++ exercise3/app.js
@@ -1,11 +1,15 @@
-var express = require('express');
-var session = require('express-session');
+var express = require('express'),
+	bodyParser = require('body-parser'),
+	session = require('express-session'),
+	path = require('path'),
+	app = express(),
+	userRoutes = require('./routes/users');
 
-var path = require('path');
-var app = express();
-var userRoutes = require('./routes/users');
 // Setup serving static assets
 app.use(express.static('public'));
+
+// Use body parser
+app.use(bodyParser.urlencoded({extended: true}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -13,19 +17,26 @@ app.set('view engine', 'ejs');
 
 // Add session support
 app.use(session({
-  secret: '...', // Customize this string for security!
-  saveUninitialized: true,
-  resave: false
+	secret: 'aapNootMies', // Customize this string for security!
+	saveUninitialized: true,
+	resave: false
 }));
 
 // Setup initial routing - index
 app.use('/users', userRoutes);
 
 app.get('/', function(req, res) {
-  res.render('index', {title: 'SSS - Exercise 3'});
+	var data = {
+		title: 'SSS Exercise 3',
+		username: null
+	};
+	if (req.session.username) {
+		data.username = req.session.username;
+	}
+	res.render('index', data);
 });
 
 // Start the server
 app.listen(3000, function(){
-  console.log('App listening at http://localhost:3000');
+	console.log('App listening at http://localhost:3000');
 });
--- exercise3/routes/users.js
+++ exercise3/routes/users.js
@@ -3,17 +3,58 @@ var router = express.Router();
 
 // Create routes for the following URL's
 // [GET] /users
+router.get('/', function (req, res, next) {
+	console.log('Opened users');
+	var data = {
+		title: 'User profile',
+		username: null
+	};
+	if (req.session.username) {
+		console.log('Registered user');
+		data.username = req.session.username;
+		res.render('index', data);
+	} else {
+		console.log('Guest user');
+		res.redirect(req.baseUrl + '/login');
+	}
+	next();
+});
 // [GET] /users/login
-// [POST] /users/login
-
-router.get("/", function(req, res){
-  if(req.session.username){
-    res.send("Welcome, " + req.session.username);
-  } else {
-    // Redirect the user here
-    res.send("We should redirected to the form to login");
-  }
+router.get('/login', function (req, res) {
+	console.log('Opened users/login');
+	var data = {
+		title: 'Login',
+		req: req,
+		err: false
+	};
+	res.render('users/login', data);
 });
 
+// [POST] /users/login
+router.post('/login', function (req, res) {
+	console.log('Posted stuff to users/login');
+	var data = {
+		title: 'Login',
+		req: req,
+		username: req.body.username,
+		password: req.body.password
+	};
+	if (data.username === 'admin' && data.password === 'pass') {
+		req.session.username = data.username;
+		res.redirect(req.baseUrl + '/');
+	} else {
+		data.err = true;
+		res.render('users/login', data);
+	}
+});
+router.get('/logout', function (req, res) {
+	console.log('Logging off');
+	if (req.session.username) {
+		console.log('Unsetting session.username');
+		req.session.username = null;
+	}
+	console.log('Redirecting');
+	res.redirect('/');
+});
 
 module.exports = router;
